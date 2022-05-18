e53e525eac655902cf27a07e3c837c120e754afe
hai <2516649312@qq.com>
2022-05-19 01:25:48 +0800

    fix: 代码提交#trtc#1818188

--- yarn1/packages/components/Bar/index.vue
+++ yarn1/packages/components/Bar/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <h2>Bar_component</h2>
+  <div> 
+      <h2>Bar_component</h2>
+      <h3> {{ count }}</h3>
+      <button @click="add_count">自增</button>
+  </div>
+
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
+import add from "@mono-demo/cor-utills";
 
 export default defineComponent({
   name: 'bar',
   setup() {
     const count = ref(0);
+    const add_count = ()=> count.value = add(count.value, 1)
     return {
       count,
+      add_count,
     }
   },
 })
